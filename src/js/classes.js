5ed91ad182c91511ef1c69f97188c778a7ce239a
DanielVierh <DanielVierh@users.noreply.github.com>
2022-07-30 16:18:38 +0200

    Delete projectile

--- src/js/classes.js
+++ src/js/classes.js
@@ -37,11 +37,15 @@ class Enemy {
             x: this.position.x + this.width / 2,
             y: this.position.y + this.height / 2,
         };
+        this.radius = 50
     }
 
     draw() {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        c.fillStyle = 'red'
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        c.beginPath()
+        c.arc(this.center.x, this.center.y, this.radius,0,  Math.PI * 2)
+        c.fill()
     }
 
     update() {
@@ -70,17 +74,19 @@ class Enemy {
 }
 
 class Projectile {
-    constructor({position = {x: 0, y: 0}}) {
+    constructor({position = {x: 0, y: 0}, enemy}) {
         this.position = position;
         this.velocity = {
             x: 0,
             y: 0,
-        };
+        }
+        this.enemy = enemy
+        this.radius = 10
     }
 
     draw() {
         c.beginPath();
-        c.arc(this.position.x, this.position.y, 10, 0, Math.PI * 2);
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
         c.fillStyle = 'orange';
         c.fill();
     }
@@ -89,8 +95,8 @@ class Projectile {
         this.draw();
 
         const angle = Math.atan2(
-            enemies[0].position.y - this.position.y,
-            enemies[0].position.x - this.position.x,
+            enemies[0].center.y - this.position.y,
+            enemies[0].center.x - this.position.x,
         );
 
         this.velocity.x = Math.cos(angle)
@@ -116,6 +122,7 @@ class Building {
                     x: this.center.x,
                     y: this.center.y,
                 },
+                enemy: enemies[0]
             }),
         ];
     }
